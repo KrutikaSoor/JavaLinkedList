6ea1e758c187a2f0b488a51f153cfd175bd9a89a
KrutikaSoor <krutikasoor@gmail.com>
2024-07-28 01:30:35 +0530

    Your commit message

---
+++ deletion.java
@@ -0,0 +1,47 @@
+public class deletion {
+    private ListNode head;
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public void insertFirst(int value){
+        ListNode newNode= new ListNode(value);
+        newNode.next= head;
+        head=newNode;
+    }
+
+    public ListNode deleteFirst(){
+        if(head==null){
+            return null;
+        }
+        ListNode temp= head;
+        head=head.next;
+        temp.next=null;
+        return temp;
+
+    }
+
+    public void display(){
+        ListNode current= head;
+        while(current!=null){
+            System.out.print(current.data+" --> ");
+            current=current.next;
+        }System.out.println("null");
+    }
+
+    public static void main(String[] args){
+        deletion sll=new deletion();
+        sll.insertFirst(2);
+        sll.insertFirst(3);
+        sll.insertFirst(9);
+        sll.insertFirst(30);
+        sll.display();
+        sll.deleteFirst();
+        sll.display();
+    }
+
+}
